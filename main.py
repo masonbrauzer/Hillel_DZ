a2155e4bb16b5eb9f5524fddad603ccbf7457252
git1 <lewolevsky@gmail.com>
2025-01-14 22:20:35 +0200

    DZ2.2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-number = input('Enter a five-digit number: ')
+number = input('Напиши как ты любишь Лену: ')
 number = int(number)
-a = number % 10
+a = (number % 10)
 b = (number % 100) // 10
 c = (number % 1000) // 100
 d = (number % 10000) // 1000
 f = (number % 100000) // 10000
-print(a, b, c, d, f)
+print(a * 10000 + b * 1000 + c * 100 + d * 10 + f * 1)
+
 
 
