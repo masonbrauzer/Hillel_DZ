99b0c23a7198f16686f95233a20048083822ebcf
Lev <95275272+masonbrauzer@users.noreply.github.com>
2025-01-23 01:00:03 +0200

    Add files via upload

---
+++ 4.3.py
@@ -0,0 +1,15 @@
+import random
+lstlength = random.randint(3, 10)
+#lst = [1, 2, 3, 4, 5, 6, 7, 9]
+#lst = [1, 1, 2, 1]
+lst = [6, 3, 7]
+first = lst[0]
+second = lst[2]
+third = lst[-2]
+print([first, second, third])
+
+
+
+
+
+
