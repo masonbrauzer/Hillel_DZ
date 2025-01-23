31b3a3af8b4c18cd8e61a8c0766715dfdfeaedfa
Lev <95275272+masonbrauzer@users.noreply.github.com>
2025-01-23 09:56:50 +0200

    Update 4.3.py
    3

--- 4.3.py
+++ 4.3.py
@@ -4,10 +4,4 @@ rndlst = random.sample(range(1,40), lst)
 first = rndlst[0]
 second = rndlst[2]
 third = rndlst[-2]
-print([first, second, third])
-
-
-
-
-
-
+print(rndlst, '==', [first, second, third])
