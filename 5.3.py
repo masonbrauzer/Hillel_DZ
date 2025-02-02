fee18ab24fbe621e4ea4aca503cbe3c5a9ffed5e
Lev <95275272+masonbrauzer@users.noreply.github.com>
2025-02-02 22:32:01 +0200

    Add files via upload

---
+++ 5.3.py
@@ -0,0 +1,10 @@
+import string
+text = input('Введіть текст: ')
+t_text = text.title()
+trash = string.punctuation + string.whitespace
+text_for_result = ''
+for i in t_text:
+    if i not in trash:
+        text_for_result += i
+print('#' + text_for_result)
+
